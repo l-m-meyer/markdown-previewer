75ef56179614cb32e1821d9baa24d08ecfec61a5
Lisa Meyer <lisamermey@gmail.com>
2022-06-09 20:01:58 -0400

    Add rows and columns to container sub-heading Badge components

--- src/App.js
+++ src/App.js
@@ -15,6 +15,26 @@ export default class App extends React.Component {
               </h1>
             </div>
           </div>
+          <div className="row mt-4">
+            <div className="col md-6">
+              <div className="col text-center">
+                <h4>
+                  <Badge className="text-align-center" variant="secondary">
+                    Markdown Input
+                  </Badge>
+                </h4>
+              </div>
+            </div>
+            <div className="col md-6">
+              <div className="col text-center">
+                <h4>
+                  <Badge className="text-align-center" variant="secondary">
+                    Preview
+                  </Badge>
+                </h4>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
